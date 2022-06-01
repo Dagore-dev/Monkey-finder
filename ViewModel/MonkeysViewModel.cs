ca43aeaa268934fff80c81f7d6ce6b8b06928677
Dagore <david.gomez.redondo.dev@gmail.com>
2022-06-01 17:54:14 +0200

    Closest monkey

--- ViewModel/MonkeysViewModel.cs
+++ ViewModel/MonkeysViewModel.cs
@@ -11,12 +11,15 @@ namespace MonkeyFinder.ViewModel
     public partial class MonkeysViewModel : BaseViewModel
     {
         MonkeyService monkeyService;
+        IConnectivity connectivity;
+        IGeolocation geolocation;
         public ObservableCollection<Monkey> Monkeys { get; } = new();
-        
-        public MonkeysViewModel (MonkeyService monkeyService)
+        public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
         {
             Title = "Monkey finder";
             this.monkeyService = monkeyService;
+            this.connectivity = connectivity;
+            this.geolocation = geolocation;
         }
 
         [ICommand]
@@ -29,6 +32,11 @@ namespace MonkeyFinder.ViewModel
 
             try
             {
+                if (connectivity.NetworkAccess != NetworkAccess.Internet)
+                {
+                    await Shell.Current.DisplayAlert("Connectivity problems", "There is a problem with your internet connection.", "OK");
+                    return;
+                }
                 IsBusy = true;
                 List<Monkey> monkeys = await monkeyService.GetMonkeys();
 
@@ -63,6 +71,44 @@ namespace MonkeyFinder.ViewModel
             {
                 {"Monkey", monkey }
             });
+        }
+        [ICommand]
+        async Task GetClosestMonkeyAsync ()
+        {
+            if (IsBusy || Monkeys.Count == 0)
+            {
+                return;
+            }
+
+            try
+            {
+                Location location = await geolocation.GetLastKnownLocationAsync();
+
+                if (location == null)
+                {
+                    location = await geolocation.GetLocationAsync(new GeolocationRequest()
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(30)
+                    });
+                }
+
+                if (location == null)
+                {
+                    return;
+                }
+
+                Monkey first = Monkeys
+                    .OrderBy(m => location.CalculateDistance(m.Latitude, m.Longitude, DistanceUnits.Miles))
+                    .FirstOrDefault();
+
+                await Shell.Current.DisplayAlert("Closest monkey", $"{first.Name} in {first.Location}", "OK");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await Shell.Current.DisplayAlert("Error", $"Unable to get closest monkey: {ex.Message}", "OK");
+            }
         }
     }
 }
