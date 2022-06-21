c178b7aa723a0d56926ae8a27374cc03fe45cf93
Dagore <david.gomez.redondo.dev@gmail.com>
2022-06-21 17:40:47 +0200

    Open map in detail view

--- ViewModel/MonkeyDetailsViewModel.cs
+++ ViewModel/MonkeyDetailsViewModel.cs
@@ -9,12 +9,32 @@ namespace MonkeyFinder.ViewModel
     [QueryProperty(nameof(Monkey), "Monkey")]
     public partial class MonkeyDetailsViewModel : BaseViewModel
     {
-        public MonkeyDetailsViewModel ()
+        IMap map;
+        public MonkeyDetailsViewModel(IMap map)
         {
-
+            this.map = map;
         }
 
         [ObservableProperty]
         Monkey monkey;
+
+        [ICommand]
+        async Task OpenMapAsync ()
+        {
+            try
+            {
+                await map.OpenAsync(Monkey.Latitude, Monkey.Longitude, new MapLaunchOptions
+                {
+                    Name = Monkey.Name,
+                    NavigationMode = NavigationMode.None
+                });
+            }
+            catch (Exception ex)
+            {
+
+                Debug.WriteLine(ex.Message);
+                await Shell.Current.DisplayAlert("Error", $"Unable to open map: {ex.Message}", "OK");
+            }
+        }
     }
 }
